ddaab5af99a4d0a530ae5edda0da39b2135aa53c
a.sohan <a.sohan@ism-ukraine.com>
2016-01-14 10:16:45 +0200

    formated code

--- app/code/core/Sohan/Core/DB.php
+++ app/code/core/Sohan/Core/DB.php
@@ -17,7 +17,13 @@ class Sohan_Core_DB
     {
         $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
         try {
-            $this->_db = new PDO(Sohan::getConfigByPath('DB/DB_TYPE') . ':host=' . Sohan::getConfigByPath('DB/DB_HOST') . ';dbname=' . Sohan::getConfigByPath('DB/DB_NAME'), Sohan::getConfigByPath('DB/DB_USER'), Sohan::getConfigByPath('DB/DB_PASS'), $options);
+            $this->_db = new PDO(Sohan::getConfigByPath('DB/DB_TYPE') .
+                ':host=' .
+                Sohan::getConfigByPath('DB/DB_HOST') .
+                ';dbname=' . Sohan::getConfigByPath('DB/DB_NAME'),
+                Sohan::getConfigByPath('DB/DB_USER'),
+                Sohan::getConfigByPath('DB/DB_PASS'),
+                $options);
         } catch (PDOException $e) {
             echo '<br>' . $e->getMessage();
         }
